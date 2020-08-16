43a2571af4137d2659074ff8d1a141ec0d455e6b
Tharujith <tharuvidu@gmail.com>
2020-08-16 21:38:12 +0530

    changed second name

--- app/src/main/java/com/example/firstttt/MainActivity.java
+++ app/src/main/java/com/example/firstttt/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btn1.setText("lol");
+                btn1.setText("wpgp");
             }
         });
 
